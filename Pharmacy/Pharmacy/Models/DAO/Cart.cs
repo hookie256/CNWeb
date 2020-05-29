27fd49ab8cb9615bada2982ccddb27d3b81284e0
thanhtung0310 <60694740+thanhtung0310@users.noreply.github.com>
2020-05-29 13:09:02 +0700

    thêm các thao tác trong giỏ hàng (Cart.cs)
    Thêm, Cập nhật, Xóa, ....

--- Pharmacy/Pharmacy/Models/DAO/Cart.cs
+++ Pharmacy/Pharmacy/Models/DAO/Cart.cs
@@ -6,14 +6,83 @@ using Pharmacy.Models.EF;
 
 namespace Pharmacy.Models.DAO
 {
+    [Serializable]
     public class CartItem
     {
-        public THUOC sanPham;
-        public int soLuong;
+        public THUOC SanPham { get; set; }
+        public int SoLuong { get; set; }
     }
 
     public class Cart
     {
+        private List<CartItem> gioHang = new List<CartItem>();
 
+        public void themSP(THUOC sp, int sl)
+        {
+            CartItem dongSP = gioHang
+                .Where(p => p.SanPham.MaThuoc == sp.MaThuoc)
+                .FirstOrDefault();
+
+            if (dongSP == null)
+            {
+                gioHang.Add(new CartItem
+                {
+                    SanPham = sp,
+                    SoLuong = sl
+                });
+            }
+            else 
+            {
+                dongSP.SoLuong += sl;
+                if (dongSP.SoLuong <= 0)
+                {
+                    gioHang.RemoveAll(l => l.SanPham.MaThuoc == sp.MaThuoc);
+                }
+            }
+        }
+
+        public void capnhatSP(THUOC sp, int sl)
+        {
+            CartItem dongSP = gioHang
+                .Where(p => p.SanPham.MaThuoc == sp.MaThuoc)
+                .FirstOrDefault();
+
+            if (dongSP != null)
+            {
+                if (sl > 0)
+                {
+                    dongSP.SoLuong = sl;
+                }
+                else
+                {
+                    gioHang.RemoveAll(l => l.SanPham.MaThuoc == sp.MaThuoc);
+                }
+            }
+        }
+
+        public void xoaSP(THUOC sp)
+        {
+            gioHang.RemoveAll(l => l.SanPham.MaThuoc == sp.MaThuoc);
+        }
+
+        public int? TongTien()
+        {
+            return gioHang.Sum(e => e.SanPham.DonGia * e.SoLuong);
+        }
+
+        public int? TongSL()
+        {
+            return gioHang.Sum(e => e.SoLuong);
+        }
+
+        public void XoaToanBo()
+        {
+            gioHang.Clear();
+        }
+
+        public IEnumerable<CartItem> dongSP
+        {
+            get { return gioHang; }
+        }
     }
 }
